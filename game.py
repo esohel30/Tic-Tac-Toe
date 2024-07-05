fe798c26d7345a49321ff71ab03fa9cdded36c6d
esohel30 <eric.sohel@stonybrook.edu>
2024-07-04 20:39:02 -0400

    added more logic

--- game.py
+++ game.py
@@ -9,6 +9,22 @@ def display_board():
   print(board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9")
   print("\n")
 
-current_player = "X"
+current_player = 'X'
 
-def play_game(): 
+
+def play_turn(position):
+    if board[position - 1] == '_': 
+        board[position - 1] = current_player 
+    else: 
+        print("Can't play that position. Go again Please.")
+    
+display_board()
+
+play_turn(1)
+play_turn(5)
+play_turn(9)
+play_turn(9)
+
+
+display_board()
+
